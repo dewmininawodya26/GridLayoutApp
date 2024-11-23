5d731077e720c9b04e80737f21caf16a5a481196
Dewmini Nawodya <dewmininawodya26@gmail.com>
2024-11-23 21:39:59 +0530

    commit

---
+++ app/src/main/java/com/example/assignmentgrid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.assignmentgrid
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)  // Set the layout we defined in XML
+    }
+}
